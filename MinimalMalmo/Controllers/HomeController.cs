47a25622b513c4c091dabd622b5a5b1952c1e10d
Pablo <pablogps@users.noreply.github.com>
2017-09-22 09:22:26 +0200

    Add files via upload

---
+++ MinimalMalmo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿t using System.Web.Mvc;
+using Microsoft.Research.Malmo;
+
+namespace MinimalMalmo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CauseTrouble()
+        {
+            ViewBag.Message = "Instantiating AgentHost creates trouble when publishing.";
+            //MissionSpec mission = new MissionSpec();
+            AgentHost agentHost = new AgentHost();
+            return RedirectToAction("Success");
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
